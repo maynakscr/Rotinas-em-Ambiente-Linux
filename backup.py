97a6f8b777e5548574346978437fe7743c90351b
Mayumi Nakazato Scremin <150185457+maynakscr@users.noreply.github.com>
2024-04-08 18:03:47 -0300

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,12 @@
+import subprocess
+
+subprocess.run(["clear"])
+subprocess.run(["cd"])
+subprocess.run(["cd bin"])
+subprocess.run(["mkdir", "python"])
+subprocess.run(["cd python"])
+subprocess.run(["vi backup.sh"])
+subprocess.run(["i"])
+
+subprocess.run([KeyboardInterrupt])
+subprocess.run([":x"])
